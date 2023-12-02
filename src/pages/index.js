e67529aab0d9ec51e70759f0e3530616ee9b454d
Ariford17 <seinmyo.smh178@gmail.com>
2023-12-03 03:21:42 +0630

    Updating process

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import Seo from "../components/seo"
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <p>I'm making this for testing the Gatsby.</p>
+      <StaticImage
+        alt="Castle with beautiful secenario"
+        src="../images/anime-aesthetic-wallpapers.jpg"
+      />
     </Layout>
   )
 }
 
-export const Head = () => <title>Home</title>
+export const Head = () => <Seo title="Home" />
 
 export default IndexPage
